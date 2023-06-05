9408326529ce38b441d1b041c81e75babc39ef17
adminuser <adminuser@kali.mvm>
2023-06-05 12:44:31 +0530

    demo

--- copy_train_sr.py
+++ copy_train_sr.py
@@ -6,26 +6,11 @@ import argparse
 from utils import charbonnier_loss, CharBonnierLoss, psnr_sr, PSNR
 from dataloaders import SRDataLoader
 
-parser = argparse.ArgumentParser()
 
-parser.add_argument('--lr', type=float, default=1e-4)
-parser.add_argument('--loss_function', type=str, default="l2")
-parser.add_argument('--n_epochs', type=int, default=200)
-parser.add_argument('--batch_size', type=int, default=32)
-parser.add_argument('--checkpoint_filepath', type=str, default="checkpoint/saved/super_resolution/")
-parser.add_argument('--num_rrg', type=int, default=3)
-parser.add_argument('--num_mrb', type=int, default=2)
-parser.add_argument('--num_channels', type=int, default=64)
-parser.add_argument('--summary', type=bool, default=False)
-parser.add_argument('--store_model_summary', type=bool, default=False)
-parser.add_argument('--scale_factor', type=int, default=4)
-parser.add_argument('--downgrade', type=str, default="bicubic")
 
-args = parser.parse_args()
-
-def train():
+def train(custom_training=True, epochs=1):
     train_loader = SRDataLoader(
-            scale=args.scale_factor,            
+            scale=4,            
             downgrade=args.downgrade,
             subset='train'
         )      
@@ -55,13 +40,9 @@ def train():
             scale_factor=args.scale_factor
         )
 
-    if args.summary:
-        model.summary()
+    
 
-    if args.store_model_summary:
-        tf.keras.utils.plot_model(to_file="mirnet_super_resolution.png")
-
-    optimizer = keras.optimizers.Adam(learning_rate=args.lr)
+    optimizer = keras.optimizers.Adam(learning_rate=1e-4)
 
     early_stopping_callback = keras.callbacks.EarlyStopping(
             monitor="val_psnr_sr",
@@ -70,7 +51,7 @@ def train():
         )
 
     model_checkpoint_callback = keras.callbacks.ModelCheckpoint(
-            args.checkpoint_filepath + "/best_model.h5",
+            "checkpoint/saved/" + "/best_model.h5",
             monitor="val_psnr_sr",
             mode="max",
             save_best_only=True,
@@ -86,27 +67,50 @@ def train():
             mode='max'
         )
 
-    if args.loss_function == "charbonnier":
-        loss_func = charbonnier_loss
+ 
 
-    if args.loss_function == "l1":
-        loss_func = tf.keras.metrics.MeanAbsoluteError()
+    loss_func = tf.keras.metrics.MeanSquaredError()
+    
+    
+    if custom_training:
+        checkpoint = tf.train.Checkpoint(
+            optimizer=optimizer,
+            model=model,
+            epoch=tf.Variable(1)
+        )
+
+        manager = tf.train.CheckpointManager(
+            checkpoint,
+            directory="checkpoint/sr/",
+            max_to_keep=5
+        )
+
+        status = checkpoint.restore(manager.latest_checkpoint)
+        trainer = Trainer(
+                    model=model,
+                    loss_func=loss_func,
+                    metric_func=psnr_sr,
+                    optimizer=optimizer,
+                    ckpt=checkpoint,
+                    ckpt_manager=manager,
+                    epochs=epochs
+                )
+
+        trainer.train(train_ds, val_ds)
 
     else:
-        loss_func = tf.keras.metrics.MeanSquaredError()
+        model.compile(
+                optimizer=optimizer,
+                loss=loss_func,
+                metrics=[psnr_sr]
+            )
 
-    model.compile(
-            optimizer=optimizer,
-            loss=loss_func,
-            metrics=[val_psnr_sr]
-        )
-
-    model.fit(
-            train_ds,
-            validation_data=val_ds,
-            epochs=args.n_epochs,
-            callbacks=[early_stopping_callback, model_checkpoint_callback, reduce_lr_loss]
-        )
+        model.fit(
+                train_ds,
+                validation_data=val_ds,
+                epochs=epochs,
+                callbacks=[early_stopping_callback, model_checkpoint_callback, reduce_lr_loss]
+            )
 
 if __name__ == '__main__':
     train()
