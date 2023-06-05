73c287419357e620681ac848299d0cb1ca8024d2
Shilu <shilu842003@gmail.com>
2023-06-05 06:08:59 +0530

    updated the dataloaders

---
+++ dataloaders/utils.py
@@ -0,0 +1,48 @@
+from tensorflow import keras 
+import tensorflow as tf 
+import numpy as np 
+
+
+def random_crop(lr_img, hr_img, hr_crop_size=128):
+    lr_crop_size = hr_crop_size
+    lr_img_shape = tf.shape(lr_img)[:2]
+
+    lr_w = tf.random.uniform(shape=(), maxval=lr_img_shape[1] - lr_crop_size + 1, dtype=tf.int32)
+    lr_h = tf.random.uniform(shape=(), maxval=lr_img_shape[0] - lr_crop_size + 1, dtype=tf.int32)
+
+    hr_w = lr_w
+    hr_h = lr_h
+
+    lr_img_cropped = lr_img[lr_h:lr_h + lr_crop_size, lr_w:lr_w + lr_crop_size]
+    hr_img_cropped = hr_img[hr_h:hr_h + hr_crop_size, hr_w:hr_w + hr_crop_size]
+
+    return lr_img_cropped, hr_img_cropped
+
+
+def random_flip(lr_img, hr_img):
+    rn = tf.random.uniform(shape=(), maxval=1)
+    return tf.cond(rn < 0.5,
+                   lambda: (lr_img, hr_img),
+                   lambda: (tf.image.flip_left_right(lr_img),
+                            tf.image.flip_left_right(hr_img)))
+
+
+def random_rotate(lr_img, hr_img):
+    rn = tf.random.uniform(shape=(), maxval=4, dtype=tf.int32)
+    return tf.image.rot90(lr_img, rn), tf.image.rot90(hr_img, rn)
+
+
+def random_crop_sr(lr_img, hr_img, hr_crop_size=96, scale=2):
+    lr_crop_size = hr_crop_size // scale
+    lr_img_shape = tf.shape(lr_img)[:2]
+
+    lr_w = tf.random.uniform(shape=(), maxval=lr_img_shape[1] - lr_crop_size + 1, dtype=tf.int32)
+    lr_h = tf.random.uniform(shape=(), maxval=lr_img_shape[0] - lr_crop_size + 1, dtype=tf.int32)
+
+    hr_w = lr_w * scale
+    hr_h = lr_h * scale
+
+    lr_img_cropped = lr_img[lr_h:lr_h + lr_crop_size, lr_w:lr_w + lr_crop_size]
+    hr_img_cropped = hr_img[hr_h:hr_h + hr_crop_size, hr_w:hr_w + hr_crop_size]
+
+    return lr_img_cropped, hr_img_cropped
