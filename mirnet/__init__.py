d162faaf24429432629b1b1b636db32b4912fb33
adminuser <adminuser@kali.mvm>
2023-06-05 08:04:16 +0530

    updated

--- mirnet/__init__.py
+++ mirnet/__init__.py
@@ -1,1 +1,2 @@
-from .models import get_enchancement_model, get_denoising_model, get_super_resolution_model
+#from .models import get_enchancement_model, get_denoising_model, get_super_resolution_model
+from .mir_backbone import MIRNet
