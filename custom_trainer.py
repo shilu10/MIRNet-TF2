228aed281a96cdf4144f74742798af974cbe1574
adminuser <adminuser@kali.mvm>
2023-06-05 08:36:54 +0530

    updated

--- custom_trainer.py
+++ custom_trainer.py
@@ -34,7 +34,6 @@ class Trainer:
             loss_val = self.loss_func(pred_image_batch, target_img_batch)
             print(loss_val)
         params = self.model.trainable_variables
-        print(params, 'params')
         grads = tape.gradient(loss_val, params)
         
         self.optimizer.apply_gradients(zip(grads, params))
